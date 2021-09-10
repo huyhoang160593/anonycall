a778a5fc2636151c3ed8cd8298194cc203126302
Chu Huy Hoang <huyhoang160593@yahoo.com>
2021-09-10 08:06:57 +0700

    create fragment host and add the first fragment to the app

---
+++ app/src/main/java/com/example/anonycall/fragments/WelcomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.anonycall.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.anonycall.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
